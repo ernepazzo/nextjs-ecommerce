aa85b30119c3b590d6d88d65c5be195a02dfad82
Ernesto Mejias <99213980+ernepazzo@users.noreply.github.com>
2023-01-15 19:02:21 -0500

    secret

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BASE_URL: "http://localhost:3000",
-    // "MONGODB_URL": "mongodb+srv://ernepazzo:3rn3st1c0.12@cluster0.ecxug.mongodb.net/nextjs-ecommerce?retryWrites=true&w=majority"
     MONGODB_URL: "mongodb://localhost:27017/nextjs-ecommerce",
     ACCESS_TOKEN_SECRET: "Lc8ud5m-<FvuT;w6}9h&`?YLw>m3Fjj4,@][:nV?:Sd.Dj3%K",
     REFRESH_TOKEN_SECRET:
